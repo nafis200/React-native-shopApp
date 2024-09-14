598a17a9664b842d7ed4fffa5873500cc775b0da
nafis200 <nafisahamed14@gmail.com>
2024-09-14 15:55:30 +0600

    fontiso

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,8 +1,8 @@
 
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import Fontisto from 'react-native-vector-icons/Fontisto'
 import { StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Header from '../components/Header';
@@ -11,6 +11,14 @@ const HomeScreen = () => {
   return (
     <LinearGradient colors={['#FDF0F3','#FFFBFC']} style={styles.conatiner}>
        <Header/>
+       <Text style={styles.matchText}>Match Your Style</Text>
+       <View style={styles.inputcontainer}>
+           <View>
+           <Fontisto name={"search"} size={26} color={"#C0C0C0"}
+           />
+           </View>
+           <TextInput style={styles.textinput}/>
+       </View>
      </LinearGradient>
   )
 }
@@ -23,7 +31,23 @@ const styles = StyleSheet.create({
    },
    texts:{
     color:'black'
-   }
+   },
+   matchText:{
+     fontSize:28,
+     color:'black',
+     marginTop:20
+   },
+   inputcontainer:{
+      backgroundColor:'#FFFFFF',
+      height:48,
+      borderRadius:12,
+      alignItems:'center',
+      flexDirection:'row',
+    },
+    textinput:{
+        flex:1,
+        borderWidth:1,
+    }
 })
 
 export default HomeScreen
