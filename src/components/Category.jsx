37027d14f3c626db7cc5756d4b830b704fa1da29
nafis200 <nafisahamed14@gmail.com>
2024-09-14 19:47:56 +0600

    done is done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,13 @@
 
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const Category = ({item,selectedCategory,setSelectedCategory}) => {
  
   return (
-    <View>
-      <Text style={styles.categoryText}>{item}</Text>
-    </View>
+    <TouchableOpacity onPress={()=>setSelectedCategory(item)}>
+      <Text style={[styles.categoryText, selectedCategory===item && {color:'#FFFFFF',backgroundColor:'#E96E6E'}]}>{item}</Text>
+    </TouchableOpacity>
   )
 }
 
@@ -17,7 +17,8 @@ const styles = StyleSheet.create({
     categoryText:{
         fontSize:16,
         fontWeight:"600",
-        backgroundColor:'#E96E6E',
+        // backgroundColor:'#E96E6E',
+        backgroundColor:'#938F8F',
         color:'white',
         padding:20,
         textAlign:'center',
