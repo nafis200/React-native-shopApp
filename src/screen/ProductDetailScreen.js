0d6d510bfa576d98cb058925ab7466a7dbde75d0
nafis200 <nafisahamed14@gmail.com>
2024-09-17 00:01:23 +0600

    scrollview

--- src/screen/ProductDetailScreen.js
+++ src/screen/ProductDetailScreen.js
@@ -1,108 +1,105 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import LinearGradient from "react-native-linear-gradient";
+import Header from "../components/Header";
+const ImageUrl =
+  "https://res.cloudinary.com/dlc5c1ycl/image/upload/v1710567612/qichw3wrcioebkvzudib.png";
 
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import LinearGradient from 'react-native-linear-gradient'
-import Header from '../components/Header'
-const ImageUrl = "https://res.cloudinary.com/dlc5c1ycl/image/upload/v1710567612/qichw3wrcioebkvzudib.png"
-
-const sizes = ["S","M","L","XL"]
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
- 
-  const [selectedSize,setSelectedSize] = useState(null)
-  console.log(selectedSize)
+  const [selectedSize, setSelectedSize] = useState(null);
+  console.log(selectedSize);
   return (
-    <LinearGradient colors={['#FDF0F3','#FFFBFC']} style={styles.container} >
-       
+    <LinearGradient colors={["#FDF0F3", "#FFFBFC"]} style={styles.container}>
       <View style={styles.HeaderContainer}>
-         <Header/>
-      </View> 
-      <Image source={{uri: ImageUrl}} style={styles.coverimage} />
+        <Header />
+      </View>
+      <Image source={{ uri: ImageUrl }} style={styles.coverimage} />
 
       <View style={styles.contentContainer}>
-         <Text style={[styles.title,{color:'black'}]}>Winter Coat</Text>
-         <Text style={[styles.price,{color:'black'}]}>$35.00</Text>
+        <Text style={[styles.title, { color: "black" }]}>Winter Coat</Text>
+        <Text style={[styles.price, { color: "black" }]}>$35.00</Text>
       </View>
 
       <View style={styles.sizeContainer}>
-      
-       <Text style={[styles.sizetext,{color:'black'},styles.title]}>
-        
-        {
-            sizes.map((size,index)=>{
-                return(
-                    <TouchableOpacity
-                     onPress={()=>{
-                        setSelectedSize(size)
-                     }}  style={styles.sizeValueContainer} key={index}>
-                        <Text 
-  style={[
-    styles.title, 
-    styles.sizeValue, 
-    selectedSize === size && { color: '#E55B5B' }
-  ]}
->
-  {size}
-</Text>
-
-                    </TouchableOpacity>
-                )
-            })
-        }
-
-       </Text>
- 
+        <Text style={[styles.sizetext, { color: "black" }, styles.title]}>
+          {sizes.map((size, index) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedSize(size);
+                }}
+                style={styles.sizeValueContainer}
+                key={index}
+              >
+                <Text
+                  style={[
+                    styles.title,
+                    styles.sizeValue,
+                    selectedSize === size && { color: "#E55B5B" },
+                  ]}
+                >
+                  {size}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </Text>
       </View>
 
-    </LinearGradient>
-  )
-}
+      <Text style={[styles.title, { color: "black"},styles.colorText]}>Colors</Text>
 
-export default ProductDetailScreen
+    </LinearGradient>
+  );
+};
+
+export default ProductDetailScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    },
-    HeaderContainer:{
-        padding:20
-    },
-    coverimage:{
-        width:"100%",
-        height:420
-    },
-    contentContainer:{
-        flexDirection:"row",
-        justifyContent:'space-between',
-        marginHorizontal:20,
-        marginVertical:20
-    },
-    title:{
-        fontSize:20,
-        color:'#444444'
-    },
-    price:{
-        fontSize:20,
-        color:'#4D4C4C'
-    },
-    sizetext:{
-       marginHorizontal:20
-    },
-    sizeContainer:{
-
-        marginHorizontal:20,
-    },
-    sizeValueContainer:{
-        height:36,
-        width:36,
-        borderRadius:18,
-        backgroundColor:'white',
-        rowGap:10,
-        gap:10,
-        
-    },
-    sizeValue:{
-         fontSize:18,
-         
-    }
-})
+  container: {
+    flex: 1,
+  },
+  HeaderContainer: {
+    padding: 20,
+  },
+  coverimage: {
+    width: "100%",
+    height: 420,
+  },
+  contentContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 20,
+    color: "#444444",
+  },
+  price: {
+    fontSize: 20,
+    color: "#4D4C4C",
+  },
+  sizetext: {
+    marginHorizontal: 20,
+  },
+  sizeContainer: {
+    marginHorizontal: 20,
+  },
+  sizeValueContainer: {
+    height: 36,
+    width: 36,
+    borderRadius: 18,
+    backgroundColor: "white",
+    rowGap: 10,
+    gap: 10,
+  },
+  sizeValue: {
+    fontSize: 18,
+  },
+  colorText:{
+    marginHorizontal:20,
+    marginTop:10
+  }
+});
