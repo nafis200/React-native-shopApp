3224f29fc3069d943ba0714f103b7a26de04807f
nafis200 <nafisahamed14@gmail.com>
2024-09-16 12:00:04 +0600

    heart icon

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
 
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 const ProductCard = () => {
-  return (
+  const [isLiked, setIsLiked] = useState(false)
+   return(
     <SafeAreaView style={styles.container}>
       <ScrollView>
       <Image source={require("../assets/girl2.png")} style={styles.coverImage} />
@@ -11,6 +13,15 @@ const ProductCard = () => {
       <Text style={styles.title}>Jackson Jeans</Text>
       <Text style={styles.price}>$45</Text>
       </View>
+      
+       <Pressable onPress={()=> setIsLiked(!isLiked)} style={styles.likeContainer}>
+
+         {
+            isLiked ? <AntDesign name={"heart"} size={20} color={"red"} /> : <AntDesign name={"hearto"} size={20} color={"red"} />
+         }
+
+       </Pressable>
+
       </ScrollView>
     </SafeAreaView>
   )
@@ -28,7 +39,8 @@ const styles = StyleSheet.create({
     },
     container:{
         flex:1,
-        marginTop:20
+        marginTop:20,
+        position:'relative'
     },
     title:{
         color:'black',
@@ -42,5 +54,16 @@ const styles = StyleSheet.create({
     },
     content:{
         paddingLeft:15
+    },
+    likeContainer:{
+      height:34,
+      width:34, 
+      backgroundColor:'white',
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:17,
+      position:'absolute',
+      top:20,
+      right:10, 
     }
 })
