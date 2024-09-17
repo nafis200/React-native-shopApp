b9c29ad11692be37b516ead3a2e7844f114209ba
nafis200 <nafisahamed14@gmail.com>
2024-09-17 15:51:15 +0600

    size25

--- src/components/Cartcard.jsx
+++ src/components/Cartcard.jsx
@@ -2,6 +2,7 @@
 
 import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
 const ImageUrl =
   "https://res.cloudinary.com/dlc5c1ycl/image/upload/v1710567612/qichw3wrcioebkvzudib.png";
 const Cartcard = () => {
@@ -14,6 +15,7 @@ const Cartcard = () => {
       <Text style={[{color:'black'}]}>$39.5</Text>
       </View>
       <Text style={{color:'black'}}>CartCard</Text>
+      <FontAwesome6 name={"trash"} color={"#F68CB5"} size={25} />
     </View>
   )
 }
